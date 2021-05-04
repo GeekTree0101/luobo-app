3cdc6ad148b754a12d08dfb0f25a94d1d3767fc6
Geektree0101 <h2s1880@gmail.com>
2021-05-04 23:21:30 +0900

    add article repository

---
+++ lib/repository/ArticleRepository.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:luobo_app/api/networking.dart';
+import 'package:luobo_app/api/request/ArticlesRequest.dart';
+import 'package:luobo_app/api/response/ArticlesResponse.dart';
+
+abstract class ArticleRepositoryLogic {
+
+  Future<ArticleResponse> getArticles(ArticlesRequest req);
+}
+
+class ArticleRepository implements ArticleRepositoryLogic {
+
+  NetworkingLogic _networking;
+
+  ArticleRepository(NetworkingLogic networking) {
+    this._networking = networking;
+  }
+
+  @override
+  Future<ArticleResponse> getArticles(ArticlesRequest req) {
+
+    return this._networking.fetch(req).then((res) {
+      final dynamic jsonBody = jsonDecode(res.body);
+      return jsonBody.map((json) => ArticleResponse.fromJson(json));
+    });
+  }
+}
