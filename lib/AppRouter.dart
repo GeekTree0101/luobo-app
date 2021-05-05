5d186b2cdb064549233a9329894ec96f03ade7a2
Geektree0101 <h2s1880@gmail.com>
2021-05-05 16:20:26 +0900

    add chagne notifier provider onto main

--- lib/AppRouter.dart
+++ lib/AppRouter.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:luobo_app/scene/home/HomeModel.dart';
 import 'package:luobo_app/scene/main/MainView.dart';
 import 'package:provider/provider.dart';
 
 import 'AppDependency.dart';
 
 class AppRouter {
-  
   AppDependency _dependency;
 
   AppRouter(AppDependency dependency) {
@@ -14,7 +14,14 @@ class AppRouter {
 
   Map<String, Widget Function(BuildContext)> routes() {
     return {
-      "/": (context) => new MainView()
+      "/": (context) => MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (context) => new HomeModel(_dependency.articleRepository),
+              ),
+            ],
+            builder: (context, child) => new MainView(),
+          )
     };
   }
 }
