0ecfe8103d98437196d4cabe7ad8037c01fb0d17
Geektree0101 <h2s1880@gmail.com>
2021-05-05 14:23:14 +0900

    add article repository onto dependency

--- lib/AppDependency.dart
+++ lib/AppDependency.dart
@@ -1,5 +1,9 @@
 
+import 'package:luobo_app/repository/ArticleRepository.dart';
+
 class AppDependency {
 
+  ArticleRepositoryLogic articleRepository;
 
+  AppDependency({this.articleRepository});
 }
