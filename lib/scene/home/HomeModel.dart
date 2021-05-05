cf12593a5be255f9463f9c82fba6799a75adebb4
Geektree0101 <h2s1880@gmail.com>
2021-05-05 14:24:55 +0900

    add HomeModel & View

---
+++ lib/scene/home/HomeModel.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+class HomeModel extends ChangeNotifier {
+  
+}
---
+++ lib/scene/home/HomeView.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+
+  @override
+  HomeView({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => HomeState();
+}
+
+class HomeState extends State<HomeView> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+  }
+}
